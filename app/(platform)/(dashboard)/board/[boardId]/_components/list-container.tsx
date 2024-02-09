5b1a260e38383ca379650df3d1a824ac3eb658f3
fred <fredy.rodriguez@pucp.pe>
2024-02-09 18:30:04 -0500

    Fix type on list-container for deployment

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { DragDropContext, Droppable } from "@hello-pangea/dnd";
-import { List } from "@prisma/client";
-import { ListForm } from "./list-form";
-import { useEffect, useState } from "react";
-import { ListItem } from "./list-item";
-import { Card } from "@prisma/client";
 import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+
+import { ListWithCards } from "@/types";
 import { useAction } from "@/hooks/use-action";
 import { updateListOrder } from "@/actions/update-list-order";
 import { updateCardOrder } from "@/actions/update-card-order";
+
+import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
+
 interface ListContainerProps {
-  data: List[];
+  data: ListWithCards[];
   boardId: string;
-}
+};
 
 function reorder<T>(list: T[], startIndex: number, endIndex: number) {
   const result = Array.from(list);
@@ -21,8 +23,12 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
   result.splice(endIndex, 0, removed);
 
   return result;
-}
-export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+};
+
+export const ListContainer = ({
+  data,
+  boardId,
+}: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
 
   const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
@@ -54,7 +60,7 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
       return;
     }
 
-    // drop in the same position
+    // if dropped in the same position
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
@@ -62,66 +68,62 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
       return;
     }
 
-    // user moves a list
-
+    // User moves a list
     if (type === "list") {
-      const items = reorder(orderedData, source.index, destination.index).map(
-        (item, index) => ({ ...item, order: index })
-      );
+      const items = reorder(
+        orderedData,
+        source.index,
+        destination.index,
+      ).map((item, index) => ({ ...item, order: index }));
 
-      executeUpdateListOrder({ items, boardId });
       setOrderedData(items);
-      // Trigger Server Action
+      executeUpdateListOrder({ items, boardId });
     }
 
-    // user moves a card
+    // User moves a card
     if (type === "card") {
       let newOrderedData = [...orderedData];
 
       // Source and destination list
-      const sourceList = newOrderedData.find(
-        (list) => list.id === source.droppableId
-      );
-      const destList = newOrderedData.find(
-        (list) => list.id === destination.droppableId
-      );
+      const sourceList = newOrderedData.find(list => list.id === source.droppableId);
+      const destList = newOrderedData.find(list => list.id === destination.droppableId);
 
       if (!sourceList || !destList) {
         return;
       }
 
       // Check if cards exists on the sourceList
-
       if (!sourceList.cards) {
         sourceList.cards = [];
       }
+
       // Check if cards exists on the destList
       if (!destList.cards) {
         destList.cards = [];
       }
 
-      // Moving the cards in the same list
-
+      // Moving the card in the same list
       if (source.droppableId === destination.droppableId) {
-        const reorderedCards: Card[] = reorder(
+        const reorderedCards = reorder(
           sourceList.cards,
           source.index,
-          destination.index
+          destination.index,
         );
 
         reorderedCards.forEach((card, idx) => {
           card.order = idx;
         });
 
-        sourceList.cards= reorderedCards;
+        sourceList.cards = reorderedCards;
+
+        setOrderedData(newOrderedData);
         executeUpdateCardOrder({
           boardId: boardId,
           items: reorderedCards,
         });
-        setOrderedData(newOrderedData);
-        // TODO: Trigger Server Action
-        // user moves the card to another list
+        // User moves the card to another list
       } else {
+        // Remove card from the source list
         const [movedCard] = sourceList.cards.splice(source.index, 1);
 
         // Assign the new listId to the moved card
@@ -130,34 +132,41 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
         // Add card to the destination list
         destList.cards.splice(destination.index, 0, movedCard);
 
-        sourceList.cards.forEach((card: any, idx: number) => {
+        sourceList.cards.forEach((card, idx) => {
           card.order = idx;
         });
 
         // Update the order for each card in the destination list
-        destList.cards.forEach((card: any, idx: number) => {
+        destList.cards.forEach((card, idx) => {
           card.order = idx;
         });
+
+        setOrderedData(newOrderedData);
         executeUpdateCardOrder({
           boardId: boardId,
           items: destList.cards,
         });
-        setOrderedData(newOrderedData);
-        // TODO: Trigger Server Action
       }
     }
-  };
+  }
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="lists" type="list" direction="horizontal">
         {(provided) => (
-          <ol
+          <ol 
             {...provided.droppableProps}
-            ref={provided.innerRef}
+            ref={provided.innerRef}  
             className="flex gap-x-3 h-full"
           >
             {orderedData.map((list, index) => {
-              return <ListItem key={list.id} index={index} data={list} />;
+              return (
+                <ListItem
+                  key={list.id}
+                  index={index}
+                  data={list}
+                />
+              )
             })}
             {provided.placeholder}
             <ListForm />
